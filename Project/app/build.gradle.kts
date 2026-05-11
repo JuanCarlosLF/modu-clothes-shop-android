3ec2e3b7f23160bad1b23df1999d2b84fdbbb6aa
Juan Carlos López Forés <jlopezf@laberit.com>
2026-05-11 12:49:11 +0000

    [MDU-21]: Add Hilt dependencies
    * [MDU-21]: Add Hilt dependencies
    
    * [MDU-21]: Remove unnecessary spaces
    
    * Merged develop into feature/MDU-21-add-hilt-dependencies
    
    
    Approved-by: Antonio Esteban
    Approved-by: Hugo Montón Blanco

--- Project/app/build.gradle.kts
+++ Project/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -48,4 +50,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    // HILT
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
