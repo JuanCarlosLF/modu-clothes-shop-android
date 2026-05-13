810b4c2ef808e97d8366a2a5cc6402b644e7ab5c
Juan Carlos López Forés <jlopezf@laberit.com>
2026-05-13 15:24:57 +0200

    [MDU-27]: Update naming

--- Project/app/src/main/java/com/example/modu/data/dataSource/remote/product/dto/ProductWrapperDto.kt
+++ Project/app/src/main/java/com/example/modu/data/dataSource/remote/product/dto/ProductWrapperDto.kt
@@ -2,7 +2,8 @@ package com.example.modu.data.dataSource.remote.product.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class ProductWrapper(
-    @SerializedName("data") val products: List<ProductDto>,
+data class ProductWrapperDto(
+    @SerializedName("data")
+    val products: List<ProductDto>,
     val meta: MetaDataDto
 )
