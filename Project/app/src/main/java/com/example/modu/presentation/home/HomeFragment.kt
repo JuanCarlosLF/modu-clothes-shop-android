6ba452c7cc135fd6a29ed50de0d91dc38e99ed9c
Juan Carlos López Forés <jlopezf@laberit.com>
2026-05-13 07:53:34 +0200

    [MDU-27]: Add home navigation

--- Project/app/src/main/java/com/example/modu/presentation/home/HomeFragment.kt
+++ Project/app/src/main/java/com/example/modu/presentation/home/HomeFragment.kt
@@ -1,6 +1,6 @@
-package com.example.modu.presentation
+package com.example.modu.presentation.home
 
-import com.example.modu.R
 import androidx.fragment.app.Fragment
+import com.example.modu.R
 
 class HomeFragment: Fragment(R.layout.fragment_home)
