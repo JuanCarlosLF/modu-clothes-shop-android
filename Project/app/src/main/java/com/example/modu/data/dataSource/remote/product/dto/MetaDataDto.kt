c610623e2b93e2fc5cf7390b282d2cf554b6502c
Juan Carlos López Forés <jlopezf@laberit.com>
2026-05-19 11:28:04 +0200

    [MDU-29]: Add null safety

--- Project/app/src/main/java/com/example/modu/data/dataSource/remote/product/dto/MetaDataDto.kt
+++ Project/app/src/main/java/com/example/modu/data/dataSource/remote/product/dto/MetaDataDto.kt
@@ -3,8 +3,8 @@ package com.example.modu.data.dataSource.remote.product.dto
 import com.google.gson.annotations.SerializedName
 
 data class MetaDataDto(
-    val page: Int,
-    val size: Int,
+    val page: Int? = null,
+    val size: Int? = null,
     @SerializedName("has_next")
-    val hasNext: Boolean
+    val hasNext: Boolean? = null
 )
