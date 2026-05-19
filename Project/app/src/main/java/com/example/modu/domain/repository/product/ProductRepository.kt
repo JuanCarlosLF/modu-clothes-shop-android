1abc49d38a8ff98cb0b0e30b1c0f92ec807e7909
Juan Carlos López Forés <jlopezf@laberit.com>
2026-05-19 14:50:38 +0000

    [MDU-29]: Add Filter View xml Layout. Set navigation between Home and Filter Screens. Update ProductRepository imports
    * [MDU-29]: Add Filter View. Set navigation between Home and Filter Screens. Update ProductRepository imports
    
    * [MDU-29]: Replace match_parent for 0dp on constrained views
    
    * [MDU-29]: Extract hardcoded values to resource files
    
    * [MDU-29]:Update images ids naming from icon_x_x to image_x_x
    
    * [MDU-29]: Extract hardcoded seekbar values to integer resources
    
    * [MDU-29]: Extract hardcoded stroke width value to dimens.xml
    
    
    Approved-by: Antonio Esteban Lorenzo
    Approved-by: Hugo Montón Blanco

--- Project/app/src/main/java/com/example/modu/domain/repository/product/ProductRepository.kt
+++ Project/app/src/main/java/com/example/modu/domain/repository/product/ProductRepository.kt
@@ -1,7 +1,7 @@
-package com.example.modu.domain.repository.products
+package com.example.modu.domain.repository.product
 
-import com.example.modu.domain.entity.product.Product
 import com.example.modu.domain.entity.product.Category
+import com.example.modu.domain.entity.product.Product
 
 interface ProductRepository {
     suspend fun getProductsBy() : List<Product>
