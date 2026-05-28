e36b4298b2fb052414e3b1d52d8cbb751e03a837
Juan Carlos López Forés <jlopezf@laberit.com>
2026-05-28 11:21:16 +0200

    [MDU-34]: Refactor to implement alerts in CartItem object

--- Project/app/src/main/java/com/example/modu/data/repository/cart/Mappers.kt
+++ Project/app/src/main/java/com/example/modu/data/repository/cart/Mappers.kt
@@ -1,22 +1,8 @@
 package com.example.modu.data.repository.cart
 
 import com.example.modu.data.dataSource.local.database.cart.dbo.CartItemDbo
-import com.example.modu.data.dataSource.remote.cart.dto.CartDto
 import com.example.modu.data.dataSource.remote.cart.dto.CartItemDto
-import com.example.modu.data.dataSource.remote.cart.dto.InsufficientStockAlertDto
-import com.example.modu.data.dataSource.remote.cart.dto.PriceAlertItemDto
-import com.example.modu.data.dataSource.remote.cart.dto.PriceChangedAlertDto
-import com.example.modu.data.dataSource.remote.cart.dto.StockAlertItemDto
-import com.example.modu.data.dataSource.remote.cart.dto.VariantAvailabilityAlertDto
-import com.example.modu.data.dataSource.remote.cart.dto.VariantAvailabilityItemDto
-import com.example.modu.domain.entity.cart.Cart
 import com.example.modu.domain.entity.cart.CartItem
-import com.example.modu.domain.entity.cart.InsufficientStockAlert
-import com.example.modu.domain.entity.cart.PriceAlertItem
-import com.example.modu.domain.entity.cart.PriceChangedAlert
-import com.example.modu.domain.entity.cart.StockAlertItem
-import com.example.modu.domain.entity.cart.VariantAvailabilityAlert
-import com.example.modu.domain.entity.cart.VariantAvailabilityItem
 
 internal fun CartItemDto.toDomain(): CartItem {
     return CartItem(
@@ -62,7 +48,10 @@ internal fun CartItem.toDbo(): CartItemDbo {
         currentStock = this.currentStock,
         quantity = this.quantity,
         unitPrice = this.unitPrice,
-        totalPrice = this.totalPrice
+        totalPrice = this.totalPrice,
+        oldPriceAlert = this.oldPriceAlert,
+        stockAlertAvailable = this.stockAlertAvailable,
+        isAvailableAlert = this.isAvailableAlert
     )
 }
 
@@ -74,55 +63,9 @@ internal fun CartItemDbo.toDomain(): CartItem {
         currentStock = this.currentStock,
         quantity = this.quantity,
         unitPrice = this.unitPrice,
-        totalPrice = this.totalPrice
-    )
-}
-
-internal fun CartDto.toDomain(): Cart {
-    return Cart(
-        createdAt = requireNotNull(this.createdAt),
-        updatedAt = requireNotNull(this.updatedAt),
-        items = this.cartItems?.map { it.toDomain() } ?: emptyList(),
-        priceAlert = this.priceChangedAlert?.toDomain(),
-        stockAlert = this.insufficientStockAlert?.toDomain()
-    )
-}
-
-internal fun PriceChangedAlertDto.toDomain(): PriceChangedAlert =
-    PriceChangedAlert(
-        items = requireNotNull(this.cartItems).map { it.toDomain() }
-    )
-
-internal fun PriceAlertItemDto.toDomain(): PriceAlertItem {
-    return PriceAlertItem(
-        productVariantId = this.productVariantId,
-        oldPrice = requireNotNull(this.oldPrice),
-        newPrice = requireNotNull(this.newPrice)
-    )
-}
-
-internal fun InsufficientStockAlertDto.toDomain(): InsufficientStockAlert =
-    InsufficientStockAlert(
-        items = requireNotNull(this.cartItems).map { it.toDomain() }
-    )
-
-internal fun StockAlertItemDto.toDomain(): StockAlertItem {
-    return StockAlertItem(
-        productVariantId = requireNotNull(this.productVariantId),
-        requestedQuantity = requireNotNull(this.requestedQuantity),
-        availableStock = requireNotNull(this.availableStock)
-    )
-}
-
-internal fun VariantAvailabilityAlertDto.toDomain(): VariantAvailabilityAlert =
-    VariantAvailabilityAlert(
-        cartItems = requireNotNull(this.cartItems).map { it.toDomain() }
-    )
-
-internal fun VariantAvailabilityItemDto.toDomain(): VariantAvailabilityItem {
-    return VariantAvailabilityItem(
-        cartItemId = requireNotNull(this.cartItemId),
-        productVariantId = requireNotNull(this.productVariantId),
-        isVariantAvailable = requireNotNull(this.isVariantAvailable)
+        totalPrice = this.totalPrice,
+        oldPriceAlert = this.oldPriceAlert,
+        stockAlertAvailable = this.stockAlertAvailable,
+        isAvailableAlert = this.isAvailableAlert
     )
 }
