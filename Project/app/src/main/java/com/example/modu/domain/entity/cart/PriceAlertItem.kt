b01ae3d3342974629cb0abb0b7f080ebbbbeab25
Juan Carlos López Forés <jlopezf@laberit.com>
2026-05-28 13:35:21 +0200

    [MDU-34]: Delete deprecated entities and add Cart Room entity and relation

--- Project/app/src/main/java/com/example/modu/domain/entity/cart/PriceAlertItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.modu.domain.entity.cart
-
-import java.math.BigDecimal
-
-data class PriceAlertItem(
-    val productVariantId: Int,
-    val oldPrice: BigDecimal,
-    val newPrice: BigDecimal
-)
