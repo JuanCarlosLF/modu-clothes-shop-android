ecc41673fa24be201415260d2814e469a761e264
Juan Carlos López Forés <jlopezf@laberit.com>
2026-05-28 08:22:16 +0200

    [MDU-34]: Update Mappers.kt

--- Project/app/src/main/java/com/example/modu/data/repository/cart/Mappers.kt
+++ Project/app/src/main/java/com/example/modu/data/repository/cart/Mappers.kt
@@ -4,28 +4,26 @@ import com.example.modu.data.dataSource.local.database.cart.dbo.CartItemDbo
 import com.example.modu.domain.entity.cart.CartItem
 import java.math.BigDecimal
 
-private const val BID_DECIMAL_CONVERSION_MULT = 100
-
 internal fun CartItemDbo.toDomain(): CartItem {
     return CartItem(
-        id = id,
-        productId = productId,
-        productVariantId = productVariantId,
-        currentStock = currentStock,
-        quantity = quantity,
-        unitPrice = BigDecimal.valueOf(unitPrice).divide(BigDecimal(BID_DECIMAL_CONVERSION_MULT)),
-        totalPrice = BigDecimal.valueOf(totalPrice).divide(BigDecimal(BID_DECIMAL_CONVERSION_MULT))
+        id = this.id,
+        productId = this.productId,
+        productVariantId = this.productVariantId,
+        currentStock = this.currentStock,
+        quantity = this.quantity,
+        unitPrice = this.unitPrice,
+        totalPrice = this.totalPrice
     )
 }
 
 internal fun CartItem.toDbo(): CartItemDbo {
     return CartItemDbo(
-        id = id,
-        productId = productId,
-        productVariantId = productVariantId,
-        currentStock = currentStock,
-        quantity = quantity,
-        unitPrice = unitPrice.multiply(BigDecimal(BID_DECIMAL_CONVERSION_MULT)).toLong(),
-        totalPrice = totalPrice.multiply(BigDecimal(BID_DECIMAL_CONVERSION_MULT)).toLong()
+        id = this.id,
+        productId = this.productId,
+        productVariantId = this.productVariantId,
+        currentStock = this.currentStock,
+        quantity = this.quantity,
+        unitPrice = this.unitPrice,
+        totalPrice = this.totalPrice
     )
 }
