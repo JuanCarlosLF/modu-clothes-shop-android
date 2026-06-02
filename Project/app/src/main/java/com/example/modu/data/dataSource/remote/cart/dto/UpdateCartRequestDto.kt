7d977e0f8d36b4a2356619993529f6241707cf8f
Juan Carlos López Forés <jlopezf@laberit.com>
2026-06-02 15:35:21 +0200

    [MDU-34]: Extract hardcoded value to top-level const

--- Project/app/src/main/java/com/example/modu/data/dataSource/remote/cart/dto/UpdateCartRequestDto.kt
+++ Project/app/src/main/java/com/example/modu/data/dataSource/remote/cart/dto/UpdateCartRequestDto.kt
@@ -2,7 +2,8 @@ package com.example.modu.data.dataSource.remote.cart.dto
 
 import java.math.BigDecimal
 
+private const val SHIPPING_COST_TO_BIG_DECIMAL_NUMBER = 100
 data class UpdateCartRequestDto(
-    val shippingCosts: BigDecimal = 100.toBigDecimal(),
+    val shippingCosts: BigDecimal = SHIPPING_COST_TO_BIG_DECIMAL_NUMBER.toBigDecimal(),
     val cartItems: List<CartItemDto>? = null
 )
