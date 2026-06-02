6b72b087c286a976a70b9a258ed82efbad70e571
Juan Carlos López Forés <jlopezf@laberit.com>
2026-06-02 15:47:10 +0200

    [MDU-34]: Update naming

--- Project/app/src/main/java/com/example/modu/data/dataSource/remote/cart/dto/UpdateCartRequestDto.kt
+++ Project/app/src/main/java/com/example/modu/data/dataSource/remote/cart/dto/UpdateCartRequestDto.kt
@@ -2,8 +2,8 @@ package com.example.modu.data.dataSource.remote.cart.dto
 
 import java.math.BigDecimal
 
-private const val SHIPPING_COST_TO_BIG_DECIMAL_NUMBER = 100
+private const val SHIPPING_COST_DEFAULT_VALUE = 100
 data class UpdateCartRequestDto(
-    val shippingCosts: BigDecimal = SHIPPING_COST_TO_BIG_DECIMAL_NUMBER.toBigDecimal(),
+    val shippingCosts: BigDecimal = SHIPPING_COST_DEFAULT_VALUE.toBigDecimal(),
     val cartItems: List<CartItemDto>? = null
 )
