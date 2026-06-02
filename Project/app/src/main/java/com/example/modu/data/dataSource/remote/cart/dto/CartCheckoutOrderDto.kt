bbaf95a02cd9c731fdb782d407ef8c6b66ffc7f3
Juan Carlos López Forés <jlopezf@laberit.com>
2026-06-02 15:48:43 +0200

    [MDU-34]: Update specialInstructions to be null by default

--- Project/app/src/main/java/com/example/modu/data/dataSource/remote/cart/dto/CartCheckoutOrderDto.kt
+++ Project/app/src/main/java/com/example/modu/data/dataSource/remote/cart/dto/CartCheckoutOrderDto.kt
@@ -7,7 +7,7 @@ data class CartCheckoutOrderDto(
     val id: Int? = null,
     val deviceId: String? = null,
     val createdAt: String? = null,
-    val specialInstructions: String?,
+    val specialInstructions: String? = null,
     val subtotalPrice: BigDecimal? = null,
     val shippingCosts: BigDecimal? = null,
     val totalPrice: BigDecimal? = null,
