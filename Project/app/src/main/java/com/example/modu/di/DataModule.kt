f88e7cdef74ddee1e4753872e367aebe74d8d184
Juan Carlos López Forés <jlopezf@laberit.com>
2026-06-03 08:37:55 +0200

    [MDU-34]: Update data and domain layer

--- Project/app/src/main/java/com/example/modu/di/DataModule.kt
+++ Project/app/src/main/java/com/example/modu/di/DataModule.kt
@@ -2,13 +2,15 @@ package com.example.modu.di
 
 import com.example.modu.data.dataSource.local.database.cart.CartLocalDataSource
 import com.example.modu.data.dataSource.local.database.cart.CartLocalDataSourceImpl
-import com.example.modu.data.dataSource.remote.product.ProductDataSource
-import com.example.modu.data.dataSource.remote.product.ProductRemoteDataSourceImpl
+import com.example.modu.data.dataSource.remote.cart.CartRemoteDataSource
+import com.example.modu.data.dataSource.remote.cart.CartRemoteDataSourceImpl
 import com.example.modu.data.dataSource.remote.exception.DataErrorHandlerImpl
 import com.example.modu.data.dataSource.remote.exception.ErrorHandler
-import com.example.modu.domain.repository.cart.CartRepository
+import com.example.modu.data.dataSource.remote.product.ProductDataSource
+import com.example.modu.data.dataSource.remote.product.ProductRemoteDataSourceImpl
 import com.example.modu.data.repository.cart.CartRepositoryImpl
 import com.example.modu.data.repository.product.ProductRepositoryImpl
+import com.example.modu.domain.repository.cart.CartRepository
 import com.example.modu.domain.repository.product.ProductRepository
 import dagger.Binds
 import dagger.Module
@@ -35,6 +37,10 @@ abstract class ErrorHandlerModule {
     @Binds
     @Singleton
     abstract fun bindCartLocalDataSource(dataSourceImpl: CartLocalDataSourceImpl) : CartLocalDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindCartRemoteDataSource(dataSourceImpl: CartRemoteDataSourceImpl): CartRemoteDataSource
 
     @Binds
     @Singleton
